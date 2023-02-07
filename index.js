8c71a5ced0218960ad9e4ee2d419fbc4b8f1955f
Austin Donovan <AustinLDonovan@gmail.com>
2023-02-07 15:20:15 -0500

    added psuedo code for explanation of programs execution in index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// prompt user for "head or tails"
+// computer chooses "head or tails" based on 0-1 from math.random (math.floor / round?)
+// computer will update prompt with "you guessed right, the coinflip landed on [choice]" or "Sorry, the coinflip landed on [Not your choice]" if numbers match.
+// prompt user for age.
+// if user was born before 2001 prompt will update with "You are old enough to drink in the US." 
+// if user was born after 2001 prompt will update with "Sorry, You are not old enough to drink in the US." 
+// if user was born on 2001 prompt will update with "You are old enough to drink in the US...barely."
